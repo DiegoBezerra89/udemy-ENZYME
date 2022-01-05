beca7241843978839eaa1d37cfe196a182db6122
Diego Bezerra Martins <diegobmartins21@gmail.com>
2022-01-04 23:53:08 -0300

    NEW: add remove properties plugin, and build

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  return (
+    <div className="head" data-test="component-app">
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
